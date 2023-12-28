54661827d4a1aafdf0564a5a430b97c2512afcab
zeze <thwjd2717@gmail.com>
2023-12-28 17:42:08 +0900

    refactor: 투두리스트를 자료구조를 EnumMap에서 이중리스트로 변경

--- wedding/src/main/java/project/wedding/domain/Card.java
+++ wedding/src/main/java/project/wedding/domain/Card.java
@@ -1,43 +1,61 @@
 package project.wedding.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Getter;
 
 @Getter
 public class Card {
-    private static final int MAX_TODO = 3;
-    private final int id; // cardId
+    private final int cardId;
     private String title;
-    private final TodoList todoList;
-    private final Status status; // cardStatus
+    private final List<List<Object>> todoList;
+    private final CardStatus cardStatus;
 
     public Card() {
-        this.id = CardIdMamager.getLastId();
-        this.todoList = new TodoList();
-        this.status = Status.BACKLOG;
-    }
-
-    public int getCardId() {
-        return this.id;
+        this.cardId = CardIdMamager.getLastId();
+        this.todoList = new ArrayList<>();
+        this.cardStatus = CardStatus.BACKLOG;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void writeTodo(TodoList.TodoEnum todoEnum, String todo) {
-        this.todoList.writeTodo(todoEnum, todo);
+    public void addTodo(String todoId, boolean checkStatus, String todo) {
+        List<Object> todoArray = new ArrayList<>();
+        todoArray.add(todoId);
+        todoArray.add(checkStatus);
+        todoArray.add(todo);
+        this.todoList.add(todoArray);
     }
 
-    public void changeCheckStatus(TodoList.TodoEnum todoEnum) {
-        this.todoList.changeCheckStatus(todoEnum);
+    public void changeCheckStatus(String todoId) {
+        List<Object> todoArray = findTodoList(todoId);
+        boolean checkStatus = (boolean) todoArray.get(1);
+        todoArray.set(1, !checkStatus);
+    }
+
+    public void changeTodo(String todoId, String todo) {
+        List<Object> todoArray = findTodoList(todoId);
+        todoArray.set(2, todo);
+    }
+
+    public List<Object> findTodoList(String todoId) {
+        for (List<Object> todoArray : todoList) {
+            if (todoArray.get(0).equals(todoId)) {
+                return todoArray;
+            }
+        }
+        throw new IllegalArgumentException("해당하는 todo가 없습니다.");
     }
 
     public String toString() {
         return "Card{" +
-            "id=" + id +
+            "id=" + cardId +
             ", title='" + title + '\'' +
-            ", todoMap=" + todoList.getTodoMap() +
-            ", status=" + status +
+            ", todoList=" + todoList +
+            ", status=" + cardStatus +
             '}';
     }
 }
