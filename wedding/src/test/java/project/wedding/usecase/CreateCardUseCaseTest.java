e5fad1be759a3bbc5f9a934f23cc4b5dac3d7ccc
zeze <thwjd2717@gmail.com>
2024-01-07 01:12:21 +0900

    test: 테스트 코드 수정 - 커버리지 80% 이상

--- wedding/src/test/java/project/wedding/usecase/CreateCardUseCaseTest.java
+++ wedding/src/test/java/project/wedding/usecase/CreateCardUseCaseTest.java
@@ -1,7 +1,9 @@
 package project.wedding.usecase;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -14,30 +16,24 @@ import project.wedding.domain.User;
 import project.wedding.domain.CardBoard;
 
 @ExtendWith(MockitoExtension.class)
-@org.junit.jupiter.api.DisplayName("카드를 생성합니다.")
+@DisplayName("user가 카드를 생성합니다.")
 class CreateCardUseCaseTest {
     @Mock
     CardContainerRepository cardContainerRepository;
-
 
     @Test
     void createCardUseCase() throws Exception {
 
         User user1 = new User("ODEE1");
-        User user2 = new User("ODEE2");
 
         CreateCardUseCase createCardUseCase = new CreateCardUseCase(cardContainerRepository);
 
         when(cardContainerRepository.findByUserId(user1.getUserId())).thenReturn(new CardBoard());
         createCardUseCase.createCard("ODEE1");
-        createCardUseCase.createCard("ODEE1");
-        createCardUseCase.createCard("ODEE1");
 
-        when(cardContainerRepository.findByUserId(user2.getUserId())).thenReturn(new CardBoard());
-        createCardUseCase.createCard("ODEE2");
-        createCardUseCase.createCard("ODEE2");
-        createCardUseCase.createCard("ODEE2");
+        verify(cardContainerRepository, times(1)).findByUserId(user1.getUserId());
+
+        assertThat(cardContainerRepository.findByUserId(user1.getUserId()).getCardById(1).getCardId()).isEqualTo(1);
     }
 
 }
-
