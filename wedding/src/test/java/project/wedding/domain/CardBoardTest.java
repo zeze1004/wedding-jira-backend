53c7922c2ed62569a06b1728bb8d0808425c4e18
zeze <thwjd2717@gmail.com>
2024-01-07 00:45:31 +0900

    test: CardBoard 리팩토링에 맞춰 테스트 코드 수정 - 테스트 커버리지 80% 이상

--- wedding/src/test/java/project/wedding/domain/CardBoardTest.java
+++ wedding/src/test/java/project/wedding/domain/CardBoardTest.java
@@ -1,25 +1,72 @@
 package project.wedding.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CardBoardTest {
 
-    @DisplayName("CardBoard에서 Card 객체를 만들 때 자료구조에 카드id와 카드객체가 들어가는지 확인해본다")
+    @DisplayName("CardBoard에서 Card를 만들 수 있다.")
     @Test
-    void createCard() {
+    void checkCardId() {
+        CardBoard cardBoard = new CardBoard();
+        cardBoard.createCard();
+
+        int cardId = cardBoard.getAllCardId().get(0);
+        assertThat(cardBoard.getCardById(cardId).getCardId()).isEqualTo(cardId);
+    }
+
+    @DisplayName("CardBoard에서 Card를 만들면 Card의 제목을 기입할 수 있다.")
+    @Test
+    void addCardTitle() {
         CardBoard cardBoard = new CardBoard();
         cardBoard.createCard();
         cardBoard.createCard();
 
-        String cardListOutput =
-                "[[1: [[null, null], [null, null], [null, null]]], " +
-                "[2: [[null, null], [null, null], [null, null]]]]";
+        List<Integer> randomCardId = cardBoard.getAllCardId();
+        for (Integer cardId : randomCardId) {
+            cardBoard.getCardById(cardId).setCardTitle("카드 제목" + cardId);
+            assertThat(cardBoard.getCardById(cardId).getCardTitle()).isEqualTo("카드 제목" + cardId);
+        }
+    }
 
+    @DisplayName("cardBoard는 card 개수를 20개만 만들 수 있다.")
+    @Test
+    void createCardLimit() {
+        CardBoard cardBoard = new CardBoard();
 
-        Assertions.assertThat(cardBoard.getAllValues()).isEqualTo(cardListOutput);
+        for (int i = 0; i < 21; i++) {
+            cardBoard.createCard();
+        }
+        assertThatThrownBy(cardBoard::createCard)
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("카드는 최대 20개까지만 만들 수 있습니다.");
+    }
 
+    @DisplayName("모든 카드들을 조회할 수 있다.")
+    @Test
+    void getAllCards() {
+        CardBoard cardBoard = new CardBoard();
+        cardBoard.createCard();
+        cardBoard.createCard();
 
+        assertThat(cardBoard.getAllCards().size()).isEqualTo(2);
+        assertThat(cardBoard.getAllCards().get(0).getCardStatus()).isEqualTo(CardStatus.BACKLOG);
+    }
+
+    @DisplayName("모든 백로그 상태의 카드들을 조회할 수 있다.")
+    @Test
+    void getAllBacklogCards() {
+        CardBoard cardBoard = new CardBoard();
+        cardBoard.createCard();
+        cardBoard.createCard();
+
+        assertThat(cardBoard.getAllBacklogCards().size()).isEqualTo(2);
+        for (Card card : cardBoard.getAllBacklogCards()) {
+            assertThat(card.getCardStatus()).isEqualTo(CardStatus.BACKLOG);
+        }
     }
 }
