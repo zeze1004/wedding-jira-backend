fd40494869ec6d9a658d0203e74f4f70f14b9328
zeze <thwjd2717@gmail.com>
2024-01-12 19:57:58 +0900

    chore: 설정파일 수정

---
+++ wedding/src/main/java/project/wedding/WeddingApplication.java
@@ -0,0 +1,11 @@
+package project.wedding;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class WeddingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingApplication.class, args);
+    }
+}
