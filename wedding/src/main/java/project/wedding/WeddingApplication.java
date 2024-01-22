6a691a7e675e945f548db4bbc941c5ff9fb7ae33
zeze <thwjd2717@gmail.com>
2024-01-22 18:35:38 +0900

    feat: 스웨거 추가

---
+++ wedding/src/main/java/project/wedding/WeddingApplication.java
@@ -0,0 +1,12 @@
+package project.wedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingApplication.class, args);
+    }
+}
