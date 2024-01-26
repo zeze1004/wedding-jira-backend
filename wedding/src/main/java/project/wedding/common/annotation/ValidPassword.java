a130f8468578e3a32c3f6b3f1fb1d0dd8afbe4db
zeze <thwjd2717@gmail.com>
2024-01-27 01:36:31 +0900

    feat: 비밀번호 커스텀 벨레데이션 어노테이션 구현

---
+++ wedding/src/main/java/project/wedding/common/annotation/ValidPassword.java
@@ -0,0 +1,19 @@
+package project.wedding.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import project.wedding.common.validator.PasswordConstraintValidator;
+
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+    String message() default "Invalid password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
