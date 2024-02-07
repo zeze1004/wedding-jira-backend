ab73cc8d85d8a2a09da835e1956d26fe9a9cd16e
zeze <thwjd2717@gmail.com>
2024-02-07 23:49:27 +0900

    refactor: 포괄적인 클래스명에서 구체적인 명칭으로 클래스명 수정

--- wedding/src/main/java/project/wedding/config/ErrorHandlerMapConfig.java
+++ wedding/src/main/java/project/wedding/config/ErrorHandlerMapConfig.java
@@ -1,6 +1,6 @@
 package project.wedding.config;
 
-import static project.wedding.common.exception.ErrorHandler.*;
+import static project.wedding.common.exception.DomainError.*;
 
 import java.util.Map;
 
@@ -8,17 +8,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
-import project.wedding.common.exception.ErrorHandler;
+import project.wedding.common.exception.DomainError;
 import project.wedding.common.exception.WeddingProjectErrorHandlerMap;
 import project.wedding.user.exception.UserError;
 
 @Configuration
 @RequiredArgsConstructor
-public class BeanConfig {
+public class ErrorHandlerMapConfig {
 
     @Bean
     public WeddingProjectErrorHandlerMap errorHandlerMap() {
-        Map<String, ErrorHandler> errorHandlerMap = Map.of(
+        Map<String, DomainError> errorHandlerMap = Map.of(
             UserError.class.getName(), USER_ERROR
         );
 
