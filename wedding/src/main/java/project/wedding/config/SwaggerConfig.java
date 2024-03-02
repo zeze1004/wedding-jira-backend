221bd068a9c80fe6a12108cc263562ece9daf72e
zeze <thwjd2717@gmail.com>
2024-03-02 09:19:09 +0900

    fix: 어노테이션 수정

--- wedding/src/main/java/project/wedding/config/SwaggerConfig.java
+++ wedding/src/main/java/project/wedding/config/SwaggerConfig.java
@@ -1,13 +1,13 @@
 package project.wedding.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
-// @Configuration
-// @EnableWebMvc
+@EnableWebMvc
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
