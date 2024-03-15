aea6daf9829b9798324778a38cbbfd8b568ae2b3
sojung.kim <thwjd2717@gmail.com>
2024-03-15 16:59:55 +0900

    refactor: persistence 구현체를 bean에 등록 Github issue #18

--- src/main/java/org/wedding/adapter/out/persistence/MybatisUserRepositoryImpl.java
+++ src/main/java/org/wedding/adapter/out/persistence/MybatisUserRepositoryImpl.java
@@ -3,9 +3,11 @@ package org.wedding.adapter.out.persistence;
 import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import org.wedding.domain.user.User;
 import org.wedding.application.port.out.repository.UserRepository;
 
+@Repository
 @Mapper
 public interface MybatisUserRepositoryImpl extends UserRepository {
     @Override
--- src/main/java/org/wedding/application/port/out/repository/UserRepository.java
+++ src/main/java/org/wedding/application/port/out/repository/UserRepository.java
@@ -2,10 +2,8 @@ package org.wedding.application.port.out.repository;
 
 import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
 import org.wedding.domain.user.User;
 
-@Repository
 public interface UserRepository {
     int save(User user);
 
