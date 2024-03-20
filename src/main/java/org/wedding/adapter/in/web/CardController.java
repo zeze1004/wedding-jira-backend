c9cd5df4413ec32e18149613e180671409dc758c
sojung.kim <thwjd2717@gmail.com>
2024-03-20 19:02:27 +0900

    feat: 카드 수정 API 구현 및 카드 생성 경로 수정

--- src/main/java/org/wedding/adapter/in/web/CardController.java
+++ src/main/java/org/wedding/adapter/in/web/CardController.java
@@ -2,12 +2,15 @@ package org.wedding.adapter.in.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.wedding.adapter.in.web.dto.CreateCardRequest;
+import org.wedding.adapter.in.web.dto.ModifyCardRequest;
 import org.wedding.application.port.in.usecase.CreateCardUseCase;
 import org.wedding.application.port.in.usecase.ModifyCardUseCase;
 import org.wedding.common.response.ApiResponse;
@@ -28,13 +31,23 @@ public class CardController {
     private final CreateCardUseCase createCardUseCase;
     private final ModifyCardUseCase modifyCardUseCase;
 
-    // 카드 생성 API
-    @PostMapping("/card")
+    @PostMapping()
     @Operation(summary = "카드 생성", description = "카드 생성")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ApiResponse<Void>> createCard(@RequestBody @Valid CreateCardRequest request) {
         createCardUseCase.createCard(request);
         ApiResponse<Void> response = ApiResponse.successApiResponse(HttpStatus.CREATED, "카드가 생성되었습니다.");
+        return new ResponseEntity<>(response, response.status());
+    }
+
+    @PatchMapping("/{cardId}")
+    @Operation(summary = "카드 수정", description = "카드 수정")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ApiResponse<Void>> modifyCard(
+        @PathVariable int cardId,
+        @RequestBody @Valid ModifyCardRequest request) {
+        modifyCardUseCase.modifyCard(cardId, request);
+        ApiResponse<Void> response = ApiResponse.successApiResponse(HttpStatus.OK, "카드가 수정되었습니다.");
         return new ResponseEntity<>(response, response.status());
     }
 
