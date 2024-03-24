de028b568127080eb0aa456b123b640597b13668
sojung.kim <thwjd2717@gmail.com>
2024-03-24 22:50:56 +0900

    feat: todo 생성 API 구현

---
+++ src/main/java/org/wedding/adapter/in/web/TodoController.java
@@ -0,0 +1,42 @@
+package org.wedding.adapter.in.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.wedding.adapter.in.web.dto.todo.CreateTodoRequest;
+import org.wedding.application.port.in.TodoUseCase;
+import org.wedding.application.port.in.command.todo.CreateTodoCommand;
+import org.wedding.common.response.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Tag(name="Todo API", description = "Todo API")
+@RestController
+@RequestMapping("/api/v1/todos")
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoUseCase todoUseCase;
+
+    @PostMapping()
+    @Operation(summary = "투두 생성", description = "투두 생성")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<ApiResponse<Void>> createTodo(@RequestBody @Valid CreateTodoRequest request) {
+
+        CreateTodoCommand command = new CreateTodoCommand(request.cardId(), request.todoItem());
+
+        todoUseCase.createTodo(command);
+
+        ApiResponse<Void> response = ApiResponse.successApiResponse(HttpStatus.CREATED, "투두가 생성되었습니다.");
+        return new ResponseEntity<>(response, response.status());
+    }
+}
