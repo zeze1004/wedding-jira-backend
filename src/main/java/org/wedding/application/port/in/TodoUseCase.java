19dcadeb46d108f8e17e3ebc273720e2e537183e
sojung.kim <thwjd2717@gmail.com>
2024-03-27 08:25:34 +0900

    feat: todo 수정 유즈케이스 추가 - 투두할일 수정 - 투두 완료 체크박스 수정
    Github issue #34

--- src/main/java/org/wedding/application/port/in/TodoUseCase.java
+++ src/main/java/org/wedding/application/port/in/TodoUseCase.java
@@ -4,10 +4,10 @@ import org.wedding.application.port.in.command.todo.CreateTodoCommand;
 
 public interface TodoUseCase {
     void createTodo(CreateTodoCommand command);
+      void updateTodoItem(UpdateTodoCommand command);
+      void updateTodoCheckStatus(UpdateTodoCommand command);
 
     /* TODO
-      void changeTodoItem(int cardId, int todoId, String todoItem);
-      void changeTodoCheckStatus(int cardId, int todoId, TodoCheckStatus todoCheckStatus);
       List<Todo> getAllTodos(int cardId);
       void deleteTodo(int cardId, int todoId);
      */
