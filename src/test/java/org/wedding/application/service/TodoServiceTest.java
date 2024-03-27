d403684c50617b12e645e099b1829717adb8c492
sojung.kim <thwjd2717@gmail.com>
2024-03-27 18:20:29 +0900

    test: todo 수정 서비스 로직 테스트 - 테스트 커버리지 80% 이상 Github issue #34

--- src/test/java/org/wedding/application/service/TodoServiceTest.java
+++ src/test/java/org/wedding/application/service/TodoServiceTest.java
@@ -2,6 +2,7 @@ package org.wedding.application.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.wedding.domain.todo.TodoCheckStatus.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +11,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
 import org.wedding.application.port.in.command.todo.CreateTodoCommand;
+import org.wedding.application.port.in.command.todo.UpdateTodoCommand;
 import org.wedding.application.port.out.repository.TodoRepository;
 import org.wedding.domain.todo.Todo;
 import org.wedding.domain.todo.exception.TodoException;
@@ -24,6 +27,7 @@ class TodoServiceTest {
     private TodoRepository todoRepository;
     private Todo todo;
     private CreateTodoCommand createCommand;
+    private UpdateTodoCommand updateCommand;
 
     @BeforeEach
     void setUp() {
@@ -34,22 +38,59 @@ class TodoServiceTest {
 
     /**
      * cardId가 존재 여부 테스트는 CardCheckAspectTest에서 진행함
+     *
      */
 
-    @DisplayName("cardId가 존재하고, 할일이 3개 미만일 때 할일 생성 성공")
+    @DisplayName("한 카드에 todo가 3개 미만일 때 최대 todo 생성 개수(3개) 이내여서 todo가 생성된다.")
     @Test
     void createTodo_Success() {
 
-        when(todoRepository.countTodosByCardId(anyInt())).thenReturn(1);
+        when(todoRepository.countTodoByCardId(anyInt())).thenReturn(1);
         todoService.createTodo(createCommand);
         verify(todoRepository).save(any(Todo.class));
     }
 
-    @DisplayName("cardId가 존재하고, 할일이 3개 이상일 때 할일 생성 실패")
+    @DisplayName("한 카드에 이미 todo가 3개 이상있으면 해당 카드에 todo 생성에 실패한다.")
     @Test
-    void createTodo_Fail_WhenMaxTodoExceed_() {
+    void createTodo_Fail_WhenMaxTodoExceed() {
 
-        when(todoRepository.countTodosByCardId(anyInt())).thenReturn(TodoService.MAX_TODOS_PER_CARD);
+        when(todoRepository.countTodoByCardId(anyInt())).thenReturn(TodoService.MAX_TODOS_PER_CARD);
         assertThrows(TodoException.class, () -> todoService.createTodo(createCommand));
+    }
+
+    @DisplayName("todo 수정시 todoId를 찾을 수 없으면 예외를 발생시킨다.")
+    @Test
+    void updateTodo_Fail_WhenTodoNotExist() {
+
+        when(todoRepository.existsByTodoId(anyInt(), anyInt())).thenReturn(false);
+        assertThrows(TodoException.class, () -> todoService.checkTodoExistence(anyInt(), anyInt()));
+    }
+
+    @DisplayName("todo 수정시 할 일과 체크 상태를 함께 변강할 수 있다.")
+    @Transactional
+    @Test
+    void updateTodo_Success_WithTodoItemAndCheckStatus() {
+
+        updateCommand = new UpdateTodoCommand(1, 1, "할 일 변경", CHECKED);
+
+        when(todoRepository.existsByTodoId(anyInt(), anyInt())).thenReturn(true);
+        when(todoRepository.findByTodoId(anyInt(), anyInt())).thenReturn(todo);
+
+        todoService.updateTodo(updateCommand);
+        verify(todoRepository).update(any(Todo.class));
+    }
+
+    @DisplayName("todo 수정시 체크 상태만 변강할 수 있다.")
+    @Transactional
+    @Test
+    void updateTodo_Success_WithTCheckStatus() {
+
+        updateCommand = new UpdateTodoCommand(1, 1, null, CHECKED);
+
+        when(todoRepository.existsByTodoId(anyInt(), anyInt())).thenReturn(true);
+        when(todoRepository.findByTodoId(anyInt(), anyInt())).thenReturn(todo);
+
+        todoService.updateTodo(updateCommand);
+        verify(todoRepository).update(any(Todo.class));
     }
 }
