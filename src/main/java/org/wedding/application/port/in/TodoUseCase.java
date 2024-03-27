f9a363a73c4fc565329c07c378635e75b86b3570
sojung.kim <thwjd2717@gmail.com>
2024-03-27 18:02:05 +0900

    feat: todo 수정 usecase 단 구현
    Github issue #34

--- src/main/java/org/wedding/application/port/in/TodoUseCase.java
+++ src/main/java/org/wedding/application/port/in/TodoUseCase.java
@@ -1,11 +1,11 @@
 package org.wedding.application.port.in;
 
 import org.wedding.application.port.in.command.todo.CreateTodoCommand;
+import org.wedding.application.port.in.command.todo.UpdateTodoCommand;
 
 public interface TodoUseCase {
     void createTodo(CreateTodoCommand command);
-      void updateTodoItem(UpdateTodoCommand command);
-      void updateTodoCheckStatus(UpdateTodoCommand command);
+    void updateTodo(UpdateTodoCommand command);
 
     /* TODO
       List<Todo> getAllTodos(int cardId);
