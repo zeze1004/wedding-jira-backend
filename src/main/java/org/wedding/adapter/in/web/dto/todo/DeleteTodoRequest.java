9a9ef9a1f459e1b261b3d542c8f666636633b62e
sojung.kim <thwjd2717@gmail.com>
2024-03-27 18:43:40 +0900

    feat: todo 삭제 클라이언트 단 구현
    Github issue #36

---
+++ src/main/java/org/wedding/adapter/in/web/dto/todo/DeleteTodoRequest.java
@@ -0,0 +1,16 @@
+package org.wedding.adapter.in.web.dto.todo;
+
+import org.wedding.domain.card.exception.CardError;
+import org.wedding.domain.todo.exception.TodoError;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record DeleteTodoRequest (
+    @NotNull
+    @PositiveOrZero(message = "CARD_ID_MUST_BE_POSITIVE", payload = CardError.class)
+    int cardId,
+    @NotNull
+    @PositiveOrZero(message = "TODO_ID_MUST_BE_POSITIVE", payload = TodoError.class)
+    int todoId
+) {}
