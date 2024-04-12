baf643d130b32cac09402d9f9374ce987298aa09
sojung.kim <thwjd2717@gmail.com>
2024-04-12 18:28:01 +0900

    feat: 카드 상태별 조회 usecase 구현 - userId로 cardIds를 찾음 - 이중 List로 리턴되는 cardIds를 양끝의 괄호를 제거해서 json -> list로 직렬화 - cardService에 cardIds와 cardStatus를 전달해서 해당 상태의 카드들만 card응답객체 리스트에 저장 - 컨트롤러에 전달할 DTO로 CardInfo를 맵핑 Github issue #73

--- src/main/java/org/wedding/application/service/CardBoardService.java
+++ src/main/java/org/wedding/application/service/CardBoardService.java
@@ -1,12 +1,24 @@
 package org.wedding.application.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.wedding.application.port.in.CardBoardUseCase;
 import org.wedding.application.port.in.command.cardboard.CreateCardBoardCommand;
+import org.wedding.application.port.in.command.cardboard.ReadCardCommand;
+import org.wedding.application.port.in.usecase.card.ReadCardUseCase;
 import org.wedding.application.port.out.repository.CardBoardRepository;
+import org.wedding.application.service.response.card.ReadCardResponse;
+import org.wedding.application.service.response.cardboard.CardInfo;
 import org.wedding.domain.cardboard.CardBoard;
 import org.wedding.domain.cardboard.exception.CardBoardError;
 import org.wedding.domain.cardboard.exception.CardBoardException;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 
@@ -15,6 +27,7 @@ import lombok.AllArgsConstructor;
 public class CardBoardService implements CardBoardUseCase {
 
     private final CardBoardRepository cardBoardRepository;
+    private final ReadCardUseCase readCardUseCase;
 
     @Override
     public void createCardBoard(CreateCardBoardCommand command) {
@@ -22,6 +35,7 @@ public class CardBoardService implements CardBoardUseCase {
         cardBoardRepository.save(cardBoard);
     }
 
+    @Transactional
     @Override
     public void addCardToCardBoard(int cardId, int userId) {
         CardBoard cardBoard = cardBoardRepository.findCardBoardByUserId(userId);
@@ -30,5 +44,44 @@ public class CardBoardService implements CardBoardUseCase {
         }
         cardBoard.addCard(cardId);
         cardBoardRepository.addCardIds(cardBoard.getCardBoardId(), cardId);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<CardInfo> readCardsByStatus(ReadCardCommand command) {
+        String cardIdsStr = cardBoardRepository
+            .findCardIdsByUserId(command.userId())
+            .toString().replaceFirst("\\[", "").replaceFirst("]", "");
+
+        List<Integer> cardIds = convertStringToList(cardIdsStr);
+
+        List<ReadCardResponse> cardResponses =
+            readCardUseCase.readCardsStausByIdsAndStatus(cardIds, command.cardStatus());
+        return toCardBoardCardInfo(cardResponses);
+    }
+
+    private List<CardInfo> toCardBoardCardInfo(List<ReadCardResponse> cardResponses) {
+        List<CardInfo> cardInfos = new ArrayList<>();
+        for (ReadCardResponse cardResponse : cardResponses) {
+            cardInfos.add(
+                new CardInfo(
+                    cardResponse.cardId(),
+                    cardResponse.cardTitle(),
+                    cardResponse.budget(),
+                    cardResponse.deadline(),
+                    cardResponse.cardStatus()
+                ));
+        }
+        return cardInfos;
+    }
+
+    public List<Integer> convertStringToList(String numberListStr) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(numberListStr, new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("CardIds로 리스트 변환이 실패했습니다.", e);
+        }
     }
 }
