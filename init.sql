9fd4f1642026130256e58afcf735519ef618e411
sojung.kim <thwjd2717@gmail.com>
2024-05-20 23:16:17 +0900

    style: 포맷팅 맞추기

--- init.sql
+++ init.sql
@@ -1,34 +1,38 @@
 -- card 테이블 생성
-CREATE TABLE IF NOT EXISTS card (
-                                    card_id INT NOT NULL PRIMARY KEY,
-                                    card_title VARCHAR(30) NOT NULL,
-                                    budget BIGINT,
-                                    deadline DATETIME,
-                                    card_status VARCHAR(10) NOT NULL
+CREATE TABLE IF NOT EXISTS card
+(
+    card_id     INT         NOT NULL PRIMARY KEY,
+    card_title  VARCHAR(30) NOT NULL,
+    budget      BIGINT,
+    deadline    DATETIME,
+    card_status VARCHAR(10) NOT NULL
 );
 
 -- cardboard 테이블 생성
-CREATE TABLE IF NOT EXISTS cardboard (
-                                         cardboard_id INT NOT NULL PRIMARY KEY,
-                                         user_id INT NOT NULL,
-                                         card_ids JSON
+CREATE TABLE IF NOT EXISTS cardboard
+(
+    cardboard_id INT NOT NULL PRIMARY KEY,
+    user_id      INT NOT NULL,
+    card_ids     JSON
 );
 
 -- todo 테이블 생성
-CREATE TABLE IF NOT EXISTS todo (
-                                    todo_id INT NOT NULL PRIMARY KEY,
-                                    todo_item VARCHAR(35) NOT NULL,
-                                    todo_check_status VARCHAR(10) NOT NULL,
-                                    card_id INT NOT NULL
+CREATE TABLE IF NOT EXISTS todo
+(
+    todo_id           INT         NOT NULL PRIMARY KEY,
+    todo_item         VARCHAR(35) NOT NULL,
+    todo_check_status VARCHAR(10) NOT NULL,
+    card_id           INT         NOT NULL
 );
 
 -- user 테이블 생성
-CREATE TABLE IF NOT EXISTS user (
-                                    email VARCHAR(30) NOT NULL,
-                                    password VARCHAR(100) NOT NULL,
-                                    name VARCHAR(20) NOT NULL,
-                                    nick_name VARCHAR(20),
-                                    created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                    id INT NOT NULL PRIMARY KEY,
-                                    partner_email VARCHAR(30)
+CREATE TABLE IF NOT EXISTS user
+(
+    email         VARCHAR(30)  NOT NULL,
+    password      VARCHAR(100) NOT NULL,
+    name          VARCHAR(20)  NOT NULL,
+    nick_name     VARCHAR(20),
+    created_date  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id            INT          NOT NULL PRIMARY KEY,
+    partner_email VARCHAR(30)
 );
