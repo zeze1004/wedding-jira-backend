5357d3cab566c9b6115543a8dbccb46f86d0f844
sojung.kim <thwjd2717@gmail.com>
2024-05-23 16:51:19 +0900

    [#97] chore: swagger UI에 토큰을 넣는 설정 추가

--- src/main/java/org/wedding/config/SwaggerConfig.java
+++ src/main/java/org/wedding/config/SwaggerConfig.java
@@ -1,26 +1,33 @@
 package org.wedding.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
-@EnableWebMvc
+@Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .components(new Components())
+            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+            .components(new Components().addSecuritySchemes("BearerAuth",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
             .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-            .title("Wedding Project API 명세서")
-            .description("Wedding Project 프로젝트에 사용되는 API 명세서")
+            .title("Wedding JIRA Planner API 명세서")
+            .description("Wedding JIRA Planner 프로젝트에 사용되는 API 명세서")
             .version("1.0.0");
     }
 }
