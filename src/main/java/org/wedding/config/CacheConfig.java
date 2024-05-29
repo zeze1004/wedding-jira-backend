a25c422c18fb633446db0c13aa0869e4202c56fd
sojung.kim <thwjd2717@gmail.com>
2024-05-29 20:58:48 +0900

    [#101] feat: 이메일 검증을 위한 로컬 캐시 config 구현

---
+++ src/main/java/org/wedding/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package org.wedding.config;
+
+import java.util.List;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(
+            List.of(new ConcurrentMapCache("verificationCode"), new ConcurrentMapCache("verifiedEmails")));
+
+        return cacheManager;
+    }
+}
